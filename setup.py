637b7b2d7462fa39e2a616ae7919b1fef2ae184e
merong564 <kmj564@sookmyung.ac.kr>
2026-03-12 13:29:31 +0900

    [Feat] 운동 자세별 교정 고도화 적용

--- setup.py
+++ setup.py
@@ -37,7 +37,9 @@ setup(
             # 🌟 누락되었던 exercise_logger_node 추가
             'exercise_logger_node = database.exercise_logger_node:main',
             'posture_corrector_all = robot_control.posture_corrector_all:main',
+            'posture_corrector_lr = robot_control.posture_corrector_lr:main',
             'pose_analyzer_all = object_detection.pose_analyzer_all:main',
+            'pose_analyzer_lr = object_detection.pose_analyzer_lr:main',
             'voice_assistant = voice_processing.voice_assistant:main',
             'system_controller = rehab_assist_robot.system_controller:main',
             'user_interface = database.user_interface:main',
